a9b89f5001ab2233265e71d1d30e184c2b31328b
tannu742 <tanisharajput9575@gmail.com>
2024-08-12 09:47:28 +0530

    nwe chnage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Stories, suggestions } from '../../constants/data';
 import Request from '../../components/friendrequest';
 import avatar from '../../assets/img/avatar.jpg';
 import { useLoaderData } from 'react-router-dom';
 import Posts from '../../components/posts';
+import ModalBox from '../../components/modal';
 
 const Home = () => {
+    const [show, setShow] = useState(false);
     const loader = useLoaderData();
     console.log({ loader });
 
@@ -15,25 +17,26 @@ const Home = () => {
                 <div className='row'>
                     <div className='col-8'>
                         <div className='d-flex story'>
-                        {
-                            Stories.map((item) => {
-                                return (
-                                    <div key={item.id} className='story_container text-center'>
-                                        <div className='stories'>
-                                            <img src={item.img} alt="" />
+                            <ModalBox show={show} />
+                            {
+                                Stories.map((item) => {
+                                    return (
+                                        <div key={item.id} className='story_container text-center' onClick={() => setShow(true)}>
+                                            <div className='stories' >
+                                                <img src={item.img} alt="" />
+                                            </div>
+                                            <div className='Story_text'>
+                                                <span>{item.head}</span>
+                                            </div>
                                         </div>
-                                        <div className='Story_text'>
-                                            <span>{item.head}</span>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
                         </div>
                         <div className='row'>
-                        {
-                            loader?.data ? loader.data.map((item) => <Posts key={item.id} thumbnail={item.thumbnailUrl} title={item.title} url={item.url}/>) : null
-                        }
+                            {
+                                loader?.data ? loader.data.map((item) => <Posts key={item.id} thumbnail={item.thumbnailUrl} title={item.title} url={item.url} />) : null
+                            }
                         </div>
                     </div>
                     <div className='col-4 suggestions'>

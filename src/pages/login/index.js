e4ec5449b40a2b432b88da86efcab73e79dfbd40
tannu742 <tanisharajput9575@gmail.com>
2024-08-18 18:30:37 +0530

    change

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -26,7 +26,7 @@ const Login = () => {
     useEffect(() => {
         if (actions?.data?.access_token) {
             sessionStorage.setItem('userInfo', JSON.stringify(actions?.data));
-            navigate('/home');
+            navigate('/');
         }
     }, [actions]);
 
